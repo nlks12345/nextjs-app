90e50752a69dacce6fbc9d5e689bbf98973aa38e
Nick Lim <nicklim128@gmail.com>
2025-02-12 17:40:49 +0800

    resolved deployment issue from vercel

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -10,7 +10,10 @@ import {
 import { formatCurrency } from './utils';
 // import postgres from 'postgres';
  
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
+// const sql = postgres(process.env.DATABASE_URL);
+
+
 // const client = await db.connect();
 
 export async function fetchRevenue() {
@@ -43,7 +46,7 @@ export async function fetchLatestInvoices() {
     console.log('Fetching latest invoice data...');
       await new Promise((resolve) => setTimeout(resolve, 3000));
 
-      const data = await client.sql<LatestInvoiceRaw>`
+      const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
